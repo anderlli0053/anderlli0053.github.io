55f31ee0bd8ff356eed4ed69410f195993a914be
Andrew Poženel - SDT <anderlli0053@live.com>
2025-03-20 00:21:24 +0100

    Added CD

---
+++ CD/js/main.js
@@ -0,0 +1,16 @@
+ // Written by Andrew Poženel - 2025
+
+
+
+
+function Alert() {
+  document.getElementById("posao").innerHTML = "Na Posao!";
+  
+}
+  
+
+
+function CookieWarning()
+{
+	window.alert("Z obiskom te strani se strinjate s piškotki");
+}
